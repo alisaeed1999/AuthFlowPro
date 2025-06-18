62a3403eb4ee6fe3c3ded3c81f1df38fb5731b3b
alisaeed1999 <alisaeed1999.a@gmail.com>
2025-06-18 14:37:01 +0300

    add sidebar and navigation layout

--- src/AuthFlowPro.Client/authflow-admin/src/app/app.routes.ts
+++ src/AuthFlowPro.Client/authflow-admin/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { RegisterComponent } from './auth/register/register';
 import { DashboardComponent } from './dashboard/dashboard';
+import { LayoutComponent } from './layout/layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'layout',
+    component: LayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // other protected routes like /users, /roles
+    ]
+  }
 ];
---
+++ src/AuthFlowPro.Client/authflow-admin/src/app/layout/layout.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    RouterOutlet
+  ],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class LayoutComponent {
+
+}
